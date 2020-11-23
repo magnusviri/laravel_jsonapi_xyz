4291f894becd6d89b6ae2f0cd494f0fbd7a340df
James Reynolds <magnusviri@me.com>
2020-11-23 12:53:05 -0700

    Seeder

--- database/seeders/XYZSeeder.php
+++ database/seeders/XYZSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\X;
 
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,28 @@ class XYZSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $x1 = X::create(['name' => 'x1']);
+
+        $x1->ys()->createMany([
+            ['name' => 'x1y1'],
+            ['name' => 'x1y2'],
+        ]);
+
+        $x1->zs()->createMany([
+            ['name' => 'x1z1'],
+            ['name' => 'x1z2'],
+        ]);
+
+        $x2 = X::create(['name' => 'x2']);
+
+        $x2->ys()->createMany([
+            ['name' => 'x2y1'],
+            ['name' => 'x2y2'],
+        ]);
+
+        $x2->zs()->createMany([
+            ['name' => 'x2z1'],
+            ['name' => 'x2z2'],
+        ]);
     }
 }
